1e62569eea7d8f394c47e1de26c9b4fbe49fdaa0
Fábio Santana <29023946+thesekcy@users.noreply.github.com>
2023-10-27 16:50:00 -0300

    corrige exports?

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css'
 
-export * from '@components/Button'
-export * from '@components/Tooltip'
-export * from '@components/Avatar'
+export { default as Button } from '@components/Button'
+export { default as Avatar } from '@components/Avatar'
+export { default as Tooltip } from '@components/Tooltip'
