6780d97f21f04cb25e6188453d3db8348f72fc90
Fábio Santana <29023946+thesekcy@users.noreply.github.com>
2023-11-16 10:16:00 -0300

    teste export

--- src/components/DropdownMenu/index.ts
+++ src/components/DropdownMenu/index.ts
@@ -1,1 +1,1 @@
-export { default as DropdownMenu } from './DropdownMenu';
+export * from './DropdownMenu';
--- src/components/MenuHorizontal/index.ts
+++ src/components/MenuHorizontal/index.ts
@@ -1,1 +1,1 @@
-export { default as MenuHorizontal } from './MenuHorizontal';
+export * from './MenuHorizontal';
