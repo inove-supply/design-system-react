740a94730a37a9443df29e8f2091baa559ab365e
Fábio Santana <29023946+thesekcy@users.noreply.github.com>
2023-11-20 04:51:34 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig ({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.ts"),
+      entry: resolve(__dirname, "src/index.tsx"),
       name: "i9-ds",
    		 
       fileName: "index",
