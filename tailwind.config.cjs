a42c45a30676067e7655cd545e93963fe587654e
Fábio Santana <29023946+thesekcy@users.noreply.github.com>
2023-11-20 07:02:30 -0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,29 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    
-  ],
-
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          900: "#101728",
+          800: "#072E57",
+          700: "#0B4682",
+          600: "#0E5DAE",
+          500: "#1274D9",
+          400: "#599EE4",
+          300: "#8BBBEC",
+          200: "#AECFF2",
+          100: "#C6DDF6",
+        },
+        gray: {
+          500: "#667085",
+          400: "#9BA2B0",
+          300: "#D0D5DD",
+          200: "#F6F7F9",
+          100: "#FCFCFD",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
